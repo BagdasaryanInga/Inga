25f18e3b2ba109acfbfb2306bff8c387e8fe94e8
BagdasaryanInga <ingabagdasaryan@ukr.net>
2022-10-02 12:50:15 +0300

    second

---
+++ text.py
@@ -0,0 +1,19 @@
+x  = 1
+if x == 0:
+    x+= 1
+    print(5/x)
+
+x = [1, 2]
+
+if x ==  0:
+    x = 1
+    print(' x был равен нулю' )
+elif type (x) == type(5) or type(x) == type (5.5):
+    print('х  допустимое значение')
+     
+else:
+    print ('В х недопустимый тип данных')
+    x = 1
+
+print( 100/x)
+         
